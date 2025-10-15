8bcf24bb51a3b1afec62a1e97a6bd18a5bd05442
sudang2 <dinojslee@g.skku.edu>
2025-10-15 18:53:34 +0900

    initial version of main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from typing import List
+def twoSum(nums: List[int], target: int) -> List[int]:
+    count=len(nums)
+    
+    for i in range(count):
+        for j in range(i+1,count):
+            if nums[i]+nums[j]==target:
+                ans=[i,j]
+        
+    return ans
